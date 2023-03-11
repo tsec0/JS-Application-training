899c75ad089a5eedcfbfc01756f6e24f4407141f
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-03-11 23:12:48 +0200

    Updated by adding lit-html
    Added lit-html to dropdown.js

--- client-side-rendering-exercise/04.Fill-Dropdown/dropdown.js
+++ client-side-rendering-exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,39 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from './node_modules/lit-html/lit-html.js';
+const url = "http://localhost:3030/jsonstore/advanced/dropdown";
+
+const root = document.getElementById("menu");
+const form = document.querySelector("form");
+form.addEventListener("submit", onSubmit);
+
+onLoadContent();
+async function onLoadContent(){
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const result = Object.values(data).map(item => createOptionTemplate(item));
+    render(result, root);
+}
+
+function createOptionTemplate(option){
+    return html`
+        <option value=${option._id}>${option.text}</option>
+    `;
+}
+
+function onSubmit(event){
+    event.preventDefault();
+    const value = document.getElementById("itemText").value;
+    value && addItem(value);
+}
+
+async function addItem(data) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify({text: data}),
+    });
+    form.reset();
+    onLoadContent();
 }

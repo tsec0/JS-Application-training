11391bf9610b154143b86fdfd0e083848fa23350
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-03-16 16:29:36 +0200

    api.js added
    New api.js filer for method requests!

---
+++ routing-exercise/01.Furniture/src/api/api.js
@@ -0,0 +1,58 @@
+const host = 'http://localhost:3030/';
+
+async function request(url, options){
+    try {
+        const response = await fetch(host + url, options);
+        if(!response){
+            const error = await response.json();
+            throw  new Error(error.message);
+        }
+        try {
+            const data = response.json();
+            return data;
+        } catch(error){
+            alert(error.message);
+            return error;
+        }
+    } catch(error){
+        alert(error.message);
+        return error;
+    }
+}
+
+function getOption(method, body){
+    const options = {
+        method,
+        headers: {},
+    }
+
+    const user = JSON.parse(localStorage.getItem("userData"));
+    const token = user.token;
+
+    if (token) {
+        options.headers["X-Authorization"] = token;
+    }
+
+    if(body){
+        options.headers["Content-Type"] = "Application/json";
+        options.body = JSON.stringify(body);
+    }
+
+    return options;
+}
+
+export async function get(url){
+    return await request("get", getOption("GET"));
+}
+
+export async function post(url, data){
+    return await request(url, getOption("POST", data));
+}
+
+export async function put(url, data){
+    return await request(url, getOption("PUT", data));
+}
+
+export async function delet(url){
+    return await request(url, getOption("DELETE"));
+}
--- routing-exercise/01.Furniture/src/app.js
+++ routing-exercise/01.Furniture/src/app.js
@@ -1,1 +1,19 @@
 console.log("work");
+import page from "../node_modules/page/page.mjs";
+import { catalogView } from "./views/catalog.js";
+import { createView } from "./views/create.js";
+import { detailsView } from "./views/details.js";
+import { editView } from "./views/edit.js";
+import { loginView } from "./views/login.js";
+import { myFurnitureView } from "./views/myFurniture.js";
+import { registerView } from "./views/register.js";
+
+page("/", catalogView);
+page("/catalog", catalogView);
+page("/create", createView);
+page("/details/:id", detailsView);
+page("/edit/:id", editView);
+page("/login", loginView);
+page("/register", registerView);
+page("/my-furniture", myFurnitureView);
+page("/*", catalogView);

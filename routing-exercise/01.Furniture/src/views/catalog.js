1fca72d60d1bca7fe867da761f05537fd726b40a
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-03-17 18:55:59 +0200

    Fixed login.js and added details.js and catalog.js
    Fixed issues with login.js GET/POST ; Catalog.js / Details.js

--- routing-exercise/01.Furniture/src/views/catalog.js
+++ routing-exercise/01.Furniture/src/views/catalog.js
@@ -1,8 +1,44 @@
-import { html } from "../../node_modules/lit-html/lit-html.js"
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getAllItem } from "../api/data.js";
 
-export async function catalogView(context){
-    //debugger
-    const test = html`<p>Hello</p>`;
-    context.render(test);
-    console.log("CatalogView");
+export async function catalogView(context) {
+  //debugger
+  const items = await getAllItem();
+  context.render(catalogTemp(items));
+  console.log("CatalogView");
+}
+
+function catalogTemp(data) {
+  return html`
+    <div class="container">
+      <div class="row space-top">
+        <div class="col-md-12">
+          <h1>Welcome to Furniture System</h1>
+          <p>Select furniture from the catalog to view details.</p>
+        </div>
+      </div>
+      <div class="row space-top">
+        ${Object.values(data).map((item) => createItem(item))}
+      </div>
+    </div>
+  `;
+}
+
+function createItem(itemDetails) {
+// debugger
+  return html` <div class="col-md-4">   
+    <div class="card text-white bg-primary">
+      <div class="card-body">
+        <img src=01.Furniture/${itemDetails.img} />
+        <p>${itemDetails.description}</p>
+        <footer>
+          <p>Price: <span>${itemDetails.price} $</span></p>
+        </footer>
+        <div>
+          <a href="/details/${itemDetails._id}" class="btn btn-info"> Details </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
 }
--- routing-exercise/01.Furniture/src/views/details.js
+++ routing-exercise/01.Furniture/src/views/details.js
@@ -1,3 +1,41 @@
-export async function detailsView(){
-    console.log("DetailsView");
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getItemById } from "../api/data.js";
+
+export async function detailsView(context) {
+    const id = context.params.id;
+    const item = await getItemById(id);
+    const userData = JSON.parse(sessionStorage.getItem("userData"));
+    context.render(detailsTemp(item, userData));
+}
+
+function detailsTemp(item) {
+    const itemImgPath = item.img.split("/");
+  return html`
+    <div class="row space-top">
+      <div class="col-md-12">
+        <h1>Furniture Details</h1>
+      </div>
+    </div>
+    <div class="row space-top">
+      <div class="col-md-4">
+        <div class="card text-white bg-primary">
+          <div class="card-body">
+          <img src=/01.Furniture/images/${itemImgPath[2]} />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <p>Make: <span>${item.make}</span></p>
+        <p>Model: <span>${item.model}</span></p>
+        <p>Year: <span>${item.year}</span></p>
+        <p>Description: <span>${item.description}</span></p>
+        <p>Price: <span>${item.price}</span></p>
+        <p>Material: <span>${item.material}</span></p>
+        <div>
+          <a href="”#”" class="btn btn-info">Edit</a>
+          <a href="”#”" class="btn btn-red">Delete</a>
+        </div>
+      </div>
+    </div>
+  `;
 }

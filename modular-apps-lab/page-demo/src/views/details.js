f4f46cd2cc0a9c144be04b3d4d6acfbcc94cca47
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-03-21 23:58:31 +0200

    Added new comments.js for recipes!
    Comments JS file show off!

--- modular-apps-lab/page-demo/src/views/details.js
+++ modular-apps-lab/page-demo/src/views/details.js
@@ -1,11 +1,13 @@
 import { html, until, nothing } from "../lib.js"; // until(template1, template2, template3, ..., )
 import { getById } from "../data/recipes.js";
 import { getLikesByRecipeId, likeRecipe } from "../data/likes.js";
+import { createSubmitHandler } from "../util.js";
+import { createComment, getCommentsByRecipeId } from "../data/comments.js";
 
 const asyncTemplate = (recipePromise) => html`
     ${until(recipePromise, recipeSkeleton())}`;
 
-const detailsTemplate = (recipe, likes, canLike, onLike) => html`
+const detailsTemplate = (recipe, likes, canLike, onLike, onComment, comments) => html`
     <h2>${recipe.name}</h2>
     <div>
     ${canLike ? html`<a href="javascript:void(0)" @click=${onLike}>Like</a>`: nothing}
@@ -20,6 +22,18 @@ const detailsTemplate = (recipe, likes, canLike, onLike) => html`
     <ul>
         ${recipe.steps.map(s=> html`<li>${s}</li>`)}
     </ul>
+    <h3>Comments:</h3>
+    <ul>
+        ${comments.map(com => html`<li>${com.author.username}: ${com.content}</li>`)}
+    </ul>
+    <div>
+        <form @submit=${onComment}>
+            <div>
+                <textarea width="500" rows="10" name="content"></textarea>
+            </div>
+            <button>Post Comment</button>
+        </form>
+    </div>
 `;
 
 const recipeSkeleton = () => html`
@@ -42,19 +56,28 @@ export function showDetails(context){
         userId = user._id;
     }
 
-    context.render(asyncTemplate(loadRecipe(id, userId, onLike)));
+    context.render(asyncTemplate(loadRecipe(id, userId, onLike, createSubmitHandler(onComment))));
 
     async function onLike(){
         await likeRecipe(id);
         context.page.redirect('/recipes/' + id);
     }
+
+    async function onComment({ content }){
+        // console.log(content);
+        await createComment(id, content);
+        context.page.redirect('/recipes/' + id);
+    }
 }
 
-async function loadRecipe(id, userId, onLike){
-    const {likes, canLike} = await getLikesByRecipeId(id, userId);
-
-    const recipe = await getById(id);
+async function loadRecipe(id, userId, onLike, onComment){
+    const [{likes, canLike}, comments, recipe] = await Promise.all([
+        getLikesByRecipeId(id, userId),
+        getCommentsByRecipeId(id),
+        getById(id)
+    ]);
+    
     const isOwner = recipe._ownerId == userId;
 
-    return detailsTemplate(recipe, likes, canLike && !isOwner, onLike);
+    return detailsTemplate(recipe, likes, canLike && !isOwner, onLike, onComment, comments);
 }
